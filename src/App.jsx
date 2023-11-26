d48bc13c567eaad7fe09d882683719b7782588df
Soobin Yang <syang26@wooster.edu>
2023-11-26 13:20:18 -0500

    refactor searchbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+// src/App.jsx
+import { useSelector } from 'react-redux';
 
 import GoogleBookCard from './components/googleBookCard';
-import { changeIndex, searchGoogleBooks } from './reducers/googleBooksReducer';
 import IndexButtons from './components/IndexButtons';
+import SearchBox from './components/SearchBox';
 
 function App() {
-  const dispatch = useDispatch();
   const googleBooks = useSelector(state => state.googleBooks);
-  const [searchTerm, setSearchTerm] = useState(''); // state for search term
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    dispatch(searchGoogleBooks(searchTerm));
-  }
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} />
-        <button type="submit">Search</button>
-      </form>
+      <SearchBox />
       {googleBooks &&
       <div>
         {googleBooks.map((book, i) => {
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react'
+import { searchGoogleBooks } from '../reducers/googleBooksReducer';
+
+function SearchBox () {
+    const dispatch = useDispatch();
+    const [searchTerm, setSearchTerm] = useState(''); // state for search term
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch(searchGoogleBooks(searchTerm));
+      }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} />
+            <button type="submit">Search</button>
+      </form>
+    )
+}
+
+export default SearchBox;
