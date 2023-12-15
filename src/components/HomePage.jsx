43f92ae243ed62cdbf7168809b8b6f4448525d32
Soobin Yang <syang26@wooster.edu>
2023-12-15 14:54:41 +0900

    add number of books I checked

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import Button from 'react-bootstrap/Button';
 import { loadUser, deleteExistingUser } from "../reducers/userReducer";
 
 function HomePage() {
     const user = useSelector(state => state.user);
+    const history = useSelector(state => state.user.history);
+    const wishlist = useSelector(state => state.user.wishlist);
     const dispatch = useDispatch();
     useEffect(() => {
       dispatch(loadUser());
@@ -17,10 +20,12 @@ function HomePage() {
   return (
     <>
       {user.token && 
-        <>
-          <div>Logged in as {user.username}</div>
+        <div className="text-center">
+          <div>Welcome {user.username}!!</div>
+          <div>You have read <Link to={"/history"} className="text-decoration-none text-dark fs-1">{history.length}</Link> books</div>
+          <div>You have <Link to={"/wishlist"} className="text-decoration-none text-dark fs-1">{wishlist.length}</Link> books to read</div>
           <Button variant="outline-danger" size="sm" onClick={handleDelete}>deleteUser</Button>
-        </>
+        </div>
       }
       {!user.token && <div>Description of the service</div>}
     </>
