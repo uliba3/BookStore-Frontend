54f79081643fb73347d68c81445281ce9d15d683
Soobin Yang <syang26@wooster.edu>
2023-12-16 11:06:19 +0900

    add photos to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import Button from 'react-bootstrap/Button';
 import { loadUser, deleteExistingUser } from "../reducers/userReducer";
 
 function HomePage() {
+    const navigate = useNavigate();
     const user = useSelector(state => state.user);
     const history = useSelector(state => state.user.history);
     const wishlist = useSelector(state => state.user.wishlist);
@@ -19,15 +20,35 @@ function HomePage() {
 
   return (
     <>
-      {user.token && 
+      {user.token &&
         <div className="text-center">
-          <div>Welcome {user.username}!!</div>
-          <div>You have read <Link to={"/history"} className="text-decoration-none text-dark fs-1">{history.length}</Link> books</div>
-          <div>You have <Link to={"/wishlist"} className="text-decoration-none text-dark fs-1">{wishlist.length}</Link> books to read</div>
+          <div className="h1 p-5 m-5" >Welcome {user.username}!!</div>
+          <div className='d-flex justify-content-around align-items-center'>
+            <div>You have read <Link to={"/history"} className="text-decoration-none text-dark fs-1">{history.length}</Link> books</div>
+            <img src="src\assets\fewBooksRightTransparent.png" className="w-25 h-25"/>
+          </div>
+          <div className='d-flex justify-content-around align-items-center'>
+            <img src="src\assets\fewBooksLeftTransparent.png" className="w-25 h-25"/>
+            <div>You have <Link to={"/wishlist"} className="text-decoration-none text-dark fs-1">{wishlist.length}</Link> books to read</div>
+          </div>
           <Button variant="outline-danger" size="sm" onClick={handleDelete}>deleteUser</Button>
         </div>
       }
-      {!user.token && <div>Description of the service</div>}
+      {!user.token && 
+        <div>
+          <div className='d-flex justify-content-around align-items-center p-5 m-5'>
+            <div className="h1">Easily search for books</div>
+            <img src="src\assets\bookStoreSearchTransparent.gif" className="w-50 h-50"/>
+          </div>
+          <div className='d-flex justify-content-around align-items-center p-5 m-5'>
+            <img src="src\assets\bookStoreSearchResultTransparent.gif" className="w-50 h-50"/>
+            <div className="h1">Look at the information of each book</div>
+          </div>
+          <div className='d-flex justify-content-around align-items-center p-5 m-5'>
+            <div className="h1">Add or remove the book from you wishlist or history</div>
+            <img src="src\assets\bookStoreListTransparent.gif" className="w-50 h-50"/>
+          </div>
+        </div>}
     </>
   );
 }
